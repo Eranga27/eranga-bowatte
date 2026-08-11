55be890bb42a97e06ff9cd5a97cb096518522898
Eranga Bowatte <eranbwt27@gmail.com>
2026-08-11 12:25:34 +0530

    feat: design and implement Eloquent One project showcase page

--- build_eloquent.py
+++ build_eloquent.py
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+import os
+
+html_content = """<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
@@ -455,4 +457,7 @@
   });
 </script>
 </body>
-</html>
+</html>"""
+
+with open("f:/my-portfolio/eranga-bowatte/eloquent-one.html", "w", encoding="utf-8") as f:
+    f.write(html_content)
